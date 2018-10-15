960321111204e89d61e384c6b091f410f3bbd99b
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2018-10-15 12:12:40 +0100

    Add C# Skeleton Files

---
+++ Answers/Question3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using C_Sharp_Challenge_Skeleton.Beans;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
+{
+    public class Question3
+    {
+        public static int Answer(int numOfNodes, Edge[] edgeLists)
+        {
+            //TODO: Please work out the solution;
+            return -1;
+        }
+    }
+}
