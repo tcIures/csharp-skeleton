bbe308efd4258d15a6fef2d68642b42e1fa3c184
Tudor Iures <tudor.iures@yahoo.ro>
2018-10-23 16:56:48 +0300

    Question 1 Code

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -8,6 +8,26 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question1
     {
+        public static int getAnswer(ushort[] prt)
+        {
+
+            ushort max = UInt16.MinValue;
+
+            for (int i = 0; i < prt.Length; i++)
+            {
+                for (int ii = i + 1; ii < prt.Length; ii++)
+                {
+                    ushort result = (ushort)(prt[i] ^ prt[ii]);
+                    if (result > max)
+                    {
+                        max = result;
+                    }
+                }
+            }
+
+            return max;
+        }
+
         public static int Answer(int[] portfolios)
         {
             //TODO: Please work out the solution;
